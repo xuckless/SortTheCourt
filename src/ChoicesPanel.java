06355e7cb65f1844c85198b064fdf38532a624b9
xuckless <72529941+xuckless@users.noreply.github.com>
2024-03-25 11:41:05 -0400

    Add files via upload

---
+++ src/ChoicesPanel.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class ChoicesPanel extends JPanel {
+  
+  public ChoicesPanel(){
+    this.setLayout(new GridBagLayout());
+    this.setVisible(true);
+    this.setOpaque(true);
+    this.setBackground(Color.BLACK);
+  }
+}
